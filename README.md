decb2ec7103898f4d28553966e2b11c2e861e58b
caleb adams <piepieninja@gmail.com>
2019-02-08 16:31:29 -0500

    adds more examples and begins tmr ability

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # Simple TMR Example
 Some Examples of [Triple Modular Redundancey (TMR)](https://en.wikipedia.org/wiki/Triple_modular_redundancy) for a [UGA SSRL](smallsat.uga.edu) workshop at the 4th annual [UGA Hacks](https://ugahacks.com/), the best hackathon on the planet. The slides accompanying this presentation can be found at [smallsat.uga.edu/research](smallsat.uga.edu/research)
 
-_Disclaimer:_ This is not intended for real use. This is only for demonstration purposes.
+_Disclaimer:_ This is not intended for real use. This is only for demonstration and educational purposes.
 
-### TMR
+#### Dependencies
+- numpy
+- matplotlib
+
+## TMR
 
 -
 
-### Worked Examples
+## Worked Examples
 
+In the `TMR.py`
 
--
+![carl irradiated](img/amimation.gif "Logo Title Text 1")
---
+++ TMR.py
@@ -0,0 +1,88 @@
+import random
+import time
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
+from matplotlib.pyplot import imshow, show
+from PIL import Image
+
+# ======= #
+# Globals #
+# ======= #
+
+# Single Event Upsets in ms
+# exagerated for teaching purposes
+SEU_per_ms = 1
+
+# if TMR is enabled or not
+TMR_enabled = False
+
+# TMR correction opportunties
+TMR_correction_rate = 5
+
+# the source image to be tested
+img_src = "img/carl3.png"
+
+# the figure for display
+fig = plt.figure()
+
+# load the initial image
+im = Image.open(img_src) # Can be many different formats.
+pix0 = im.load()
+pix1 = pix0
+pix2 = pix0
+
+print 'loaded ' + str(img_src) + ' of size: ' + str(im.size)  # Get the width and hight of the image for iterating over
+ani_im = plt.imshow(im, animated=True)
+
+
+
+# =================================================== #
+#              EDIT THIS FUNCTION CODE                #
+# =================================================== #
+
+def TMR():
+    # TMR code goes here
+    print 'TMR GOES HERE'
+
+
+
+# from: https://matplotlib.org/examples/animation/dynamic_image.html
+def updatefig(*args):
+    x_flip = random.randint(0,im.size[0]-1)
+    y_flip = random.randint(0,im.size[1]-1)
+    color = pix0[x_flip,y_flip]
+    #print 'Flipping bits at pix0el [' + str(x_flip) + ',' + str(y_flip) + '] ' + str(color)
+
+    # find where to flip the bit
+    c_flip = random.randint(0,2)
+    partial_color = 0
+    n = random.randint(0,7)
+    val = 2**n
+
+    # flip the bits
+    if (int(color[c_flip]) + val > 255):
+        partial_color = color[c_flip] - val
+    else:
+        partial_color = color[c_flip] + val
+
+    # the 3 flip options
+    if c_flip == 0:
+        color = (c_flip,color[1],color[2])
+    if c_flip == 1:
+        color = (color[0],c_flip,color[2])
+    if c_flip == 2:
+        color = (color[0],color[1],c_flip)
+
+    # now actually change the color!
+    pix0[x_flip,y_flip] = (color)
+    color = pix0[x_flip,y_flip]
+
+    # set and return for the animation loop
+    ani_im.set_array(im)
+    return ani_im,
+
+# show the animation
+ani = animation.FuncAnimation(fig, updatefig, interval=SEU_per_ms, frames=500, blit=True)
+ani.save('img/animation.gif', writer='imagemagick', fps=60)
+plt.show()
