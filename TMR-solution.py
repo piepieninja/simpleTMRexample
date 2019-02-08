2d48266a0c9378791ef23f8f6b87610979805080
caleb adams <piepieninja@gmail.com>
2019-02-08 18:09:55 -0500

    adds more events per second

--- TMR-solution.py
+++ TMR-solution.py
@@ -13,12 +13,13 @@ from PIL import Image
 # Single Event Upsets in ms
 # exagerated for teaching purposes
 SEU_per_ms = 1
+SEU_multi = 10
 
 # if TMR is enabled or not
 TMR_enabled = False
 
 # TMR correction opportunties
-TMR_correction_rate = 500
+TMR_correction_rate = 500*SEU_multi
 TMR_wait_count = 0
 
 # the source image to be tested
@@ -84,56 +85,57 @@ def updatefig(*args):
     global TMR_enabled, TMR_wait_count
     global pix0, pix1, pix2, pix_total
 
-    # find the location to flip a bit
-    x_flip = random.randint(0,im_total.size[0]-1)
-    y_flip = random.randint(0,im_total.size[1]-1)
+    for x in range(0,SEU_multi):
+        # find the location to flip a bit
+        x_flip = random.randint(0,im_total.size[0]-1)
+        y_flip = random.randint(0,im_total.size[1]-1)
 
-    i_f = random.randint(0,2)
-    # choose the image that will have a bit flipped
-    color = pix_total[x_flip,y_flip]
-    if (i_f == 0):
-        color = pix0[x_flip,y_flip]
-    elif (i_f == 1):
-        color = pix1[x_flip,y_flip]
-    elif (i_f == 2):
-        color = pix2[x_flip,y_flip]
+        i_f = random.randint(0,2)
+        # choose the image that will have a bit flipped
+        color = pix_total[x_flip,y_flip]
+        if (i_f == 0):
+            color = pix0[x_flip,y_flip]
+        elif (i_f == 1):
+            color = pix1[x_flip,y_flip]
+        elif (i_f == 2):
+            color = pix2[x_flip,y_flip]
 
-    # this is wher the TMR can occur
-    if (TMR_wait_count == TMR_correction_rate):
-        TMR_wait_count = 0
-        TMR()
-    else:
-        TMR_wait_count += 1
+        # this is wher the TMR can occur
+        if (TMR_wait_count == TMR_correction_rate):
+            TMR_wait_count = 0
+            TMR()
+        else:
+            TMR_wait_count += 1
 
-    # find where to flip the bit
-    c_flip = random.randint(0,2)
-    partial_color = 0
-    n = random.randint(0,7)
-    val = 2**n
+        # find where to flip the bit
+        c_flip = random.randint(0,2)
+        partial_color = 0
+        n = random.randint(0,7)
+        val = 2**n
 
-    # flip the bits
-    if (int(color[c_flip]) + val > 255):
-        partial_color = color[c_flip] - val
-    else:
-        partial_color = color[c_flip] + val
+        # flip the bits
+        if (int(color[c_flip]) + val > 255):
+            partial_color = color[c_flip] - val
+        else:
+            partial_color = color[c_flip] + val
 
-    # the 3 flip options
-    if c_flip == 0:
-        color = (c_flip,color[1],color[2])
-    elif c_flip == 1:
-        color = (color[0],c_flip,color[2])
-    elif c_flip == 2:
-        color = (color[0],color[1],c_flip)
+        # the 3 flip options
+        if c_flip == 0:
+            color = (c_flip,color[1],color[2])
+        elif c_flip == 1:
+            color = (color[0],c_flip,color[2])
+        elif c_flip == 2:
+            color = (color[0],color[1],c_flip)
 
-    # now actually change the color!
-    if (i_f == 0):
-        pix0[x_flip,y_flip] = color
-    elif (i_f == 1):
-        pix1[x_flip,y_flip] = color
-    elif (i_f == 2):
-        pix2[x_flip,y_flip] = color
-    pix_total[x_flip,y_flip] = (color)
-    color = pix_total[x_flip,y_flip]
+        # now actually change the color!
+        if (i_f == 0):
+            pix0[x_flip,y_flip] = color
+        elif (i_f == 1):
+            pix1[x_flip,y_flip] = color
+        elif (i_f == 2):
+            pix2[x_flip,y_flip] = color
+        pix_total[x_flip,y_flip] = (color)
+        color = pix_total[x_flip,y_flip]
 
     # set and return for the animation loop
     ani_im.set_array(im_total)
